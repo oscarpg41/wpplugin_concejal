e228aae3e25112604e532b8657caaf9555d2ab11
Oscar Perez <oscarpg41@gmail.com>
2015-03-27 16:36:47 +0100

    no message

--- opg_concejales.php
+++ opg_concejales.php
@@ -10,7 +10,6 @@ License: GPLv2
 */
 ?>
 <?php
-
 //Lo que hacemos es añadir los scripts necesarios para que el cargador de medios de wordpress se muestre
     function my_admin_scripts_photo_councilor() {
         wp_enqueue_script('media-upload');
@@ -26,30 +25,20 @@ License: GPLv2
         add_action('admin_print_styles', 'my_admin_styles_photo_councilor');
     }
     // cargador de medios de wordpress
-
-
     //registramos el fichero js que necesitamos
     //wp_register_script('myPluginConcejalesScript', WP_PLUGIN_URL . '/opg_concejales/opg_concejales.js');
     wp_register_script('myPluginConcejalesScript', WP_PLUGIN_URL .'/opg_concejales/opg_concejales.js', array('jquery','media-upload','thickbox'));
     wp_enqueue_script('myPluginConcejalesScript');    
-
-
     /* Con este código, se crea una linea en el menú de Administración */
     function opg_show_menu_concejal(){
         add_menu_page('Oscar Pérez Plugins','Oscar Pérez Plugins','manage_options','opg_plugins','opg_plugin_links_show_form_in_wpadmin', '', 110);
         add_submenu_page( 'opg_plugins', 'Municipal Corporation','Corporación Municipal', 'manage_options', 'opg_concejales', 'opg_plugin_concejal_show_form_in_wpadmin');
         remove_submenu_page( 'opg_plugins', 'opg_plugins' );  
     }
-
     add_action( 'admin_menu', 'opg_show_menu_concejal' );
-
-
-
     //Hook al activar y desactivar el plugin
     register_activation_hook( __FILE__, 'opg_plugin_concejal_activate' );
     register_uninstall_hook( __FILE__, 'opg_plugin_concejal_uninstall' );
-
-
     // Se crea la tabla al activar el plugin
     function opg_plugin_concejal_activate() {
         global $wpdb;
@@ -62,20 +51,15 @@ License: GPLv2
               `image` VARCHAR( 140 ) NOT NULL ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci';
         $wpdb->query($sql);
     }
-
     // Se borra la tabla al desisntalar el plugin
     function opg_plugin_concejal_uninstall() {
         global $wpdb;
         $sql = 'DROP TABLE `' . $wpdb->prefix . 'opg_plugin_concejal`';
         $wpdb->query($sql);
     }
-
-
-
     /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
         F U N C I O N E S   D E   A C C E S O   A   B A S E   D E   D A T O S
      * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
-
     //función que guarda en base de datos la información introducida en el formulario
     function opg_concejal_save($name, $email, $description, $biography, $image)
     {
@@ -84,7 +68,6 @@ License: GPLv2
             _e('cannot get \$_POST[]');
             exit;
         }
-
         $save_or_no = $wpdb->insert($wpdb->prefix . 'opg_plugin_concejal', 
             array( 'idConcejal' => NULL, 'name' => esc_js(trim ($name)), 'email' => trim ($email), 'description' => trim ($description), 'biography' => trim ($biography), 'image' => trim ($image) ),
             array( '%d', '%s', '%s', '%s', '%s', '%s' )
@@ -98,8 +81,6 @@ License: GPLv2
         }
         return true;
     }
-
-
     //función que borra un teléfono de la base de datos
     function opg_concejal_remove($id)
     {
@@ -108,7 +89,6 @@ License: GPLv2
             _e('cannot get \$_GET[]');
             exit;
         }
-
         $delete_or_no = $wpdb->delete($wpdb->prefix . 'opg_plugin_concejal', array('idConcejal' => $id), array( '%d' ) );
         if (!$delete_or_no) {
             _e('<div class="updated"><p><strong>Error. Please install plugin again</strong></p></div>');
@@ -119,7 +99,6 @@ License: GPLv2
         }
         return true;
     }
-
     //función para actualizar un teléfono
     function opg_concejal_update($id, $name, $email, $description, $biography, $image)
     {
@@ -128,7 +107,6 @@ License: GPLv2
             _e('cannot get \$_POST[]');
             exit;
         }
-
         if ( isset($image) && (strlen($image)>0) ){
             $update_or_no = $wpdb->update($wpdb->prefix . 'opg_plugin_concejal', 
                 array( 'name' => esc_js(trim ($name)), 'email' => trim ($email), 'description' => trim ($description), 'biography' => trim ($biography), 'image' => trim ($image) ),
@@ -143,7 +121,6 @@ License: GPLv2
                 array( '%s', '%s', '%s', '%s' )
             );
         }        
-
         if (!$update_or_no) {
             _e('<div class="updated"><p><strong>Error. Please install plugin again</strong></p></div>');
             return false;
@@ -153,8 +130,6 @@ License: GPLv2
         }
         return true;
     }
-
-
     //función que recupera un telefono usando el ID
     function opg_concejal_getId($id)
     {
@@ -162,18 +137,15 @@ License: GPLv2
         $row1 = $wpdb->get_row("SELECT name, email, description, biography, image  FROM " . $wpdb->prefix . "opg_plugin_concejal  WHERE idConcejal=".$id);
         return $row1;
     }
-
-
     //función que recupera los concejales guardados de la base de datos
     function opg_concejal_getData()
     {
         global $wpdb;
-
         $records = $wpdb->get_results( 'SELECT idConcejal, name, email, description, biography, image FROM ' . $wpdb->prefix . 'opg_plugin_concejal ORDER BY idConcejal' );
         if (count($records)>0){
 ?>
             <hr style="width:94%; margin:20px 0">
-	        <h2>Corporacion municipal</h2>
+            <h2>Corporacion municipal</h2>
             <table class="wp-list-table widefat manage-column" style="width:98%">            
              <thead>
                 <tr>
@@ -196,26 +168,22 @@ License: GPLv2
                     <td nowrap><?php echo( $record->email ); ?></td>
                     <td><?php echo( nl2br($record->description) ); ?></td>
                     <td><a href="admin.php?page=opg_concejales&amp;task=edit_concejal&amp;id=<?php echo( $record->idConcejal ); ?>"><img src="<?php echo WP_PLUGIN_URL.'/opg_concejales/img/modificar.png'?>" alt="Modificar"></a></td>
-                    <td><a href="#"><img src="<?php echo WP_PLUGIN_URL.'/opg_concejales/img/papelera.png'?>" alt="Borrar" id="<?php echo( $record->idConcejal ); ?>" class="btnDeleteCouncilor"></a></td>
+                    <td><a href="#"><img src="<?php echo WP_PLUGIN_URL.'/opg_aside/img/papelera.png'?>" alt="Borrar" id="<?php echo( $record->idConcejal ); ?>" class="btnDeleteCouncilor"></a></td>
                 </tr>
 <?php                
             }
         }
-
 ?>
              </tbody>
             </table>
 <?php
         return true;
     }
-
-
-
-	/*
-	   F U N C I O N   Q U E   S E   E J E C U T A   A L   A C C E D E R   A L   P L U G I N   D E S D E   A D M I N I S T R A C I O N
+    /*
+       F U N C I O N   Q U E   S E   E J E C U T A   A L   A C C E D E R   A L   P L U G I N   D E S D E   A D M I N I S T R A C I O N
        La función la definimos en la llamada add_menu_page()
-	*/
-	function opg_plugin_concejal_show_form_in_wpadmin(){
+    */
+    function opg_plugin_concejal_show_form_in_wpadmin(){
  
         $valueInputDesc  = "";
         $valueInputName  = "";
@@ -223,10 +191,7 @@ License: GPLv2
         $valueInputEmail = "";
         $valueInputBio   = "";
         $valueInputImage = "";
-
-
-	    if(isset($_POST['action']) && $_POST['action'] == 'salvaropciones'){
-
+        if(isset($_POST['action']) && $_POST['action'] == 'salvaropciones'){
             //si el input idConcejal (hidden) está vacio, se trata de un nuevo registro
             if( strlen($_POST['idConcejal']) == 0 ){
                 //guardamos el teléfono
@@ -235,7 +200,7 @@ License: GPLv2
             else{
                 opg_concejal_update($_POST['idConcejal'], $_POST['name'], $_POST['email'], $_POST['description'], $_POST['biography'], $_POST['upload_image']);
             }   
-	    }
+        }
         else{
             //recuperamos la tarea a realizar (edit o delete)
             if (isset($_GET["task"]))
@@ -247,8 +212,6 @@ License: GPLv2
                 $id = $_GET["id"];
             else
                 $id = 0;
-
-
             switch ($task) {
                 case 'edit_concejal':
                     echo("<div class='wrap'><h2>Modificar información del concejal</h2></div>"); 
@@ -314,7 +277,7 @@ License: GPLv2
 
                     <tr>
                         <td colspan='2' style='text-align:center; padding-top: 50px'>
-                            <input type='submit' value='Guardar'>
+                            <input type='submit' value='Enviar'>
                             <input type='hidden' name="idConcejal" value="<?php echo $valueInputId ?>">
                         </td>
                     </tr>
