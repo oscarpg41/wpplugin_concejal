f158aa283015063f37a542360d902b9d549193e4
oscarperezgomez <46523183+oscarperezgomez@users.noreply.github.com>
2026-09-24 18:31:12 +0200

    Modificamos el nombre,  la URI y la versión del plugin

--- corporacion-municipal.php
+++ corporacion-municipal.php
@@ -1,9 +1,9 @@
 <?php
 /**
- * Plugin Name: Corporación Municipal
- * Plugin URI: https://www.oscarperez.es/wordpress-plugins/corporacion-municipal
+ * Plugin Name: [Óscar Pérez Gómez] Corporación Municipal
+ * Plugin URI: https://github.com/oscarpg41/wpplugin_concejal
  * Description: Gestiona y muestra el listado de concejales de una corporación municipal. Usa el shortcode [corporacion_municipal] para mostrarlo en cualquier página o entrada.
- * Version: 2.0.2
+ * Version: 2.0.1
  * Author: Óscar Pérez
  * Author URI: https://www.oscarperez.es/
  * License: GPLv2 or later
@@ -15,7 +15,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'CM_VERSION', '2.0.2' );
+define( 'CM_VERSION', '2.0.1' );
 define( 'CM_PLUGIN_FILE', __FILE__ );
 define( 'CM_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 define( 'CM_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
